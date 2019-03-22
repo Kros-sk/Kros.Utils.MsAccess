7d4302f0265932f241c131e293a702265ea2e190
Lukáš Šefčík <lukas.sefcik@gmail.com>
2019-03-22 12:24:53 +0100

    Initial commit.

--- tests/Kros.Utils.UnitTests/Helpers.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Kros.Utils.UnitTests
-{
-    internal static class Helpers
-    {
-        public const string RootNamespace = "Kros.Utils.UnitTests";
-        public const string RootNamespaceResources = RootNamespace + ".Resources";
-    }
-}
